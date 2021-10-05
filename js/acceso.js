e437f5d78b14689f8d0b0f59b3146649cb70a93a
wrlocon <91978209+wrlocon@users.noreply.github.com>
2021-10-05 11:58:01 -0600

    Add files via upload

---
+++ js/acceso.js
@@ -0,0 +1,18 @@
+
+function go() {
+    if(document.miformulario.Usuario.value=='esdras' &&  document.miformulario.Contraseña.value=='examen'){
+    document.miformulario.submit();
+}
+
+
+else{
+    alert("ingrese sus usuario y contraseña correctamente.");
+}
+
+
+
+
+
+}
+
+
